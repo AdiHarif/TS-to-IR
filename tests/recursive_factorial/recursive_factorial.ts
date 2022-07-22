3f36325b1b023c1b3551a0191e30ee6454e5ab3f
Adi Harif <adi.harif@campus.technion.ac.il>
2022-07-22 19:29:16 +0300

    adjusted recursive factorial test to new wrapper design

--- tests/recursive_factorial/recursive_factorial.ts
+++ tests/recursive_factorial/recursive_factorial.ts
@@ -1,5 +1,5 @@
 
-import scanf from "scanf";
+import { strict as assert } from 'assert';
 
 function recFact(n: number): number {
 	if (n < 0){
@@ -11,6 +11,12 @@ function recFact(n: number): number {
 	return recFact(n - 1) * n;
 }
 
-function main(): void {
-	console.log(recFact(scanf("%d")));
-}
+assert(recFact(-1) ==  -1);
+assert(recFact(0) == 1);
+assert(recFact(1) == 1);
+assert(recFact(2) == 2);
+assert(recFact(3) == 6);
+assert(recFact(4) == 24);
+assert(recFact(5) == 120);
+assert(recFact(6) == 720);
+assert(recFact(10) == 3628800);

f03b52291e9b575d66b652d2be64378ce7a8dae1
Adi Harif <adi.harif@campus.technion.ac.il>
2022-07-23 03:42:14 +0300

    adjusted recursive fibonacci test to new wrapper design

--- tests/recursive_fibonacci/recursive_fibonacci.ts
+++ tests/recursive_fibonacci/recursive_fibonacci.ts
@@ -1,5 +1,5 @@
 
-import scanf from "scanf";
+import { strict as assert } from 'assert';
 
 function recFib(n: number): number {
 	if (n < 0) {
@@ -11,6 +11,13 @@ function recFib(n: number): number {
 	return (recFib(n-1) + recFib(n-2));
 }
 
-function main(): void {
-	console.log(recFib(scanf("%d")));
-}
+assert(recFib(-1) == -1);
+assert(recFib(0) == 0);
+assert(recFib(1) == 1);
+assert(recFib(2) == 1);
+assert(recFib(3) == 2);
+assert(recFib(4) == 3);
+assert(recFib(5) == 5);
+assert(recFib(6) == 8);
+assert(recFib(7) == 13);
+assert(recFib(20) == 6765);

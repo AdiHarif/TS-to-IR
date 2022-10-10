06f26c1576eaad5dda104acea1732c5792de898a
Adi Harif <adi.harif@campus.technion.ac.il>
2022-10-10 17:06:52 +0300

    added partialCompiling flag

--- src/cmd_line.ts
+++ src/cmd_line.ts
@@ -4,11 +4,13 @@ import * as cmd from "ts-command-line-args";
 export interface CommandLineArguments {
 	sourceFiles: Array<string>,
 	outputDir: string,
+	partialCompiling: boolean
 };
 
 export function parseCommandLineArgs(): CommandLineArguments {
 	return cmd.parse<CommandLineArguments>({
 		sourceFiles: { type: String, multiple: true, defaultOption: true },
 		outputDir: { type: String, alias: 'o', defaultValue: 'out' },
+		partialCompiling: { type: Boolean, defaultValue: false },
 	});
 }
